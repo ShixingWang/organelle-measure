240b9a99bff86117e7e1a242b8c157d56d46e26c
ShixingWang <wangshixing@wustl.edu>
2024-08-23 19:22:55 -0500

    boxplot for percentage difference between max normalized total volumes

--- scripts/rebuttal/manual_segment.py
+++ scripts/rebuttal/manual_segment.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from skimage import io,util,measure
 from batch_apply import batch_apply
 
+cmap = plt.get_cmap("tab10")
 
 px_x,px_y,px_z = 0.41, 0.41, 0.20
 organelles = [
@@ -19,118 +20,119 @@ organelles = [
 folder_img = Path("images/rebuttal_manual")
 folder_data = Path("data/EYrainbow_glucose_largerBF/")
 
-# %% white pixels in ImageJ uint16 images are not 'white'
+# # %% white pixels in ImageJ uint16 images are not 'white'
 
-whites = {
-    "peroxisome":   1608,
-    "vacuole":        80,
-    "ER":            179,
-    "golgi":          12,
-    "mitochondria":  255,
-    "LD":            255,
-}
+# whites = {
+#     "peroxisome":   1608,
+#     "vacuole":        80,
+#     "ER":            179,
+#     "golgi":          12,
+#     "mitochondria":  255,
+#     "LD":            255,
+# }
 
-for path_painted in folder_img.glob("painted*.tif"):
-    organelle = path_painted.stem.partition('_')[2].partition('_')[0]
+# for path_painted in folder_img.glob("painted*.tif"):
+#     organelle = path_painted.stem.partition('_')[2].partition('_')[0]
     
-    painted = io.imread(str(path_painted))
-    corrected = (painted==whites[organelle])
+#     painted = io.imread(str(path_painted))
+#     corrected = (painted==whites[organelle])
 
-    io.imsave(
-        f"{folder_img}/equal_{path_painted.name}",
-        util.img_as_ubyte(corrected)
-    )
+#     io.imsave(
+#         f"{folder_img}/equal_{path_painted.name}",
+#         util.img_as_ubyte(corrected)
+#     )
 
-# %% measure organelles
-def parse_meta_organelle(name):
-    """name is the stem of the ORGANELLE label image file."""
+# # %% measure organelles
+# def parse_meta_organelle(name):
+#     """name is the stem of the ORGANELLE label image file."""
 
-    return {
-        "experiment": "glucose",
-        "condition":  "100",
-        "hour":       3,
-        "field":      0,
-        "organelle":  name.partition("_")[2].partition("_")[0]
-    }
+#     return {
+#         "experiment": "glucose",
+#         "condition":  "100",
+#         "hour":       3,
+#         "field":      0,
+#         "organelle":  name.partition("_")[2].partition("_")[0]
+#     }
 
 
-def measure1organelle(path_in,path_cell,path_out,metadata=None):
-    # parse metadata from filename
-    name = Path(path_in).stem
+# def measure1organelle(path_in,path_cell,path_out,metadata=None):
+#     # parse metadata from filename
+#     name = Path(path_in).stem
 
-    meta = parse_meta_organelle(name) if metadata is None else metadata
+#     meta = parse_meta_organelle(name) if metadata is None else metadata
 
-    img_orga = io.imread(str(path_in))
-    img_cell = io.imread(str(path_cell))
+#     img_orga = io.imread(str(path_in))
+#     img_cell = io.imread(str(path_cell))
     
-    dfs = []
-    for cell in measure.regionprops(img_cell):
-        meta["idx-cell"] = cell.label
-        min_row, min_col, max_row, max_col = cell.bbox
-        img_orga_crop = img_orga[:,min_row:max_row,min_col:max_col]
-        img_cell_crop = cell.image
-        for z in range(img_orga_crop.shape[0]):
-            img_orga_crop[z] = img_orga_crop[z]*img_cell_crop
-        if not meta["organelle"] == "vacuole":
-            measured_orga = measure.regionprops_table(
-                img_orga_crop,
-                properties=('label','area','bbox_area','bbox')
-            )
-        else:
-            vacuole_area = 0
-            vacuole_bbox_area = 0
-            bbox0,bbox1,bbox2,bbox3,bbox4,bbox5 = 0,0,0,0,0,0
-            for z in range(img_orga_crop.shape[0]):
-                vacuole = measure.regionprops_table(
-                    img_orga_crop[z],
-                    properties=('label','area','bbox_area','bbox')
-                )
-                if len(vacuole["area"]) == 0:
-                    continue
-                if (maxblob:=max(vacuole["area"])) > vacuole_area:
-                    vacuole_area = maxblob
-                    idxblob = np.argmax(vacuole["area"])
-                    vacuole_bbox_area = vacuole["bbox_area"][idxblob]
-                    bbox0,bbox3 = z,z
-                    bbox1,bbox2,bbox4,bbox5 = [vacuole[f"bbox-{i}"][idxblob] for i in range(4)]
-            if vacuole_area==0:
-                continue
-            measured_orga = {
-                'label': [0],
-                'area':  [vacuole_area],
-                "bbox_area": [vacuole_bbox_area],
-                "bbox-0": [bbox0],
-                "bbox-1": [bbox1],
-                "bbox-2": [bbox2],
-                "bbox-3": [bbox3],
-                "bbox-4": [bbox4],
-                "bbox-5": [bbox5],
-            }
-        result = meta | measured_orga
-        dfs.append(pd.DataFrame(result))
-    if len(dfs) == 0:
-        print(f">>> {path_out} has no cells, skipped.")
-        return None
-    df_orga = pd.concat(dfs,ignore_index=True)
-    df_orga.rename(columns={'label':'idx-orga',"area":"volume-pixel",'bbox_area':'volume-bbox'},inplace=True)
-    df_orga.to_csv(str(path_out),index=False)
-    print(f">>> finished {path_out.stem}.")
-    return None
+#     dfs = []
+#     for cell in measure.regionprops(img_cell):
+#         meta["idx-cell"] = cell.label
+#         min_row, min_col, max_row, max_col = cell.bbox
+#         img_orga_crop = img_orga[:,min_row:max_row,min_col:max_col]
+#         img_cell_crop = cell.image
+#         for z in range(img_orga_crop.shape[0]):
+#             img_orga_crop[z] = img_orga_crop[z]*img_cell_crop
+#         if not meta["organelle"] == "vacuole":
+#             measured_orga = measure.regionprops_table(
+#                 img_orga_crop,
+#                 properties=('label','area','bbox_area','bbox')
+#             )
+#         else:
+#             vacuole_area = 0
+#             vacuole_bbox_area = 0
+#             bbox0,bbox1,bbox2,bbox3,bbox4,bbox5 = 0,0,0,0,0,0
+#             for z in range(img_orga_crop.shape[0]):
+#                 vacuole = measure.regionprops_table(
+#                     img_orga_crop[z],
+#                     properties=('label','area','bbox_area','bbox')
+#                 )
+#                 if len(vacuole["area"]) == 0:
+#                     continue
+#                 if (maxblob:=max(vacuole["area"])) > vacuole_area:
+#                     vacuole_area = maxblob
+#                     idxblob = np.argmax(vacuole["area"])
+#                     vacuole_bbox_area = vacuole["bbox_area"][idxblob]
+#                     bbox0,bbox3 = z,z
+#                     bbox1,bbox2,bbox4,bbox5 = [vacuole[f"bbox-{i}"][idxblob] for i in range(4)]
+#             if vacuole_area==0:
+#                 continue
+#             measured_orga = {
+#                 'label': [0],
+#                 'area':  [vacuole_area],
+#                 "bbox_area": [vacuole_bbox_area],
+#                 "bbox-0": [bbox0],
+#                 "bbox-1": [bbox1],
+#                 "bbox-2": [bbox2],
+#                 "bbox-3": [bbox3],
+#                 "bbox-4": [bbox4],
+#                 "bbox-5": [bbox5],
+#             }
+#         result = meta | measured_orga
+#         dfs.append(pd.DataFrame(result))
+#     if len(dfs) == 0:
+#         print(f">>> {path_out} has no cells, skipped.")
+#         return None
+#     df_orga = pd.concat(dfs,ignore_index=True)
+#     df_orga.rename(columns={'label':'idx-orga',"area":"volume-pixel",'bbox_area':'volume-bbox'},inplace=True)
+#     df_orga.to_csv(str(path_out),index=False)
+#     print(f">>> finished {path_out.stem}.")
+#     return None
 
-list_in   = []
-list_cell = []
-list_out  = []
-for organelle in organelles:
-    list_in.append(f"{folder_img}/label_{organelle}_EYrainbow_glu-100_field-0_crop.tif")
-    list_cell.append(f"{folder_img}/binCell_EYrainbow_glu-100_field-0_crop.tif")
-    list_out.append(f"{folder_img}/{organelle}_EYrainbow_glu-100_field-0_crop.csv")
-args = pd.DataFrame({
-    "path_in":   list_in,
-    "path_cell": list_cell,
-    "path_out":  list_out,
-})
-# %%
-batch_apply(measure1organelle,args)
+# list_in   = []
+# list_cell = []
+# list_out  = []
+# for organelle in organelles:
+#     list_in.append(f"{folder_img}/label_{organelle}_EYrainbow_glu-100_field-0_crop.tif")
+#     list_cell.append(f"{folder_img}/binCell_EYrainbow_glu-100_field-0_crop.tif")
+#     list_out.append(f"{folder_img}/{organelle}_EYrainbow_glu-100_field-0_crop.csv")
+# args = pd.DataFrame({
+#     "path_in":   list_in,
+#     "path_cell": list_cell,
+#     "path_out":  list_out,
+# })
+
+# # %%
+# batch_apply(measure1organelle,args)
 
 # %% access data of cells and organelles 
 img_cell = io.imread(str(folder_img/"binCell_EYrainbow_glu-100_field-0_crop.tif"))
@@ -209,6 +211,34 @@ pivot_orga_bycell.loc[pivot_manual_bycell_totl.index,'total'] = pivot_manual_byc
 
 df_orga_bycell = pivot_orga_bycell.reset_index()
 
+for organelle in organelles:
+    df_orga_bycell.loc[
+        df_orga_bycell["organelle"].eq(organelle)
+      & df_orga_bycell["method"].eq("ilastik"),
+        "total_normed_max"
+    ] = df_orga_bycell.loc[
+            df_orga_bycell["organelle"].eq(organelle)
+          & df_orga_bycell["method"].eq("ilastik"),
+            "total"
+    ] / df_orga_bycell.loc[
+        df_orga_bycell["organelle"].eq(organelle)
+      & df_orga_bycell["method"].eq("ilastik"),
+        "total"
+    ].max()
+    df_orga_bycell.loc[
+        df_orga_bycell["organelle"].eq(organelle)
+      & df_orga_bycell["method"].eq("manual"),
+        "total_normed_max"
+    ] = df_orga_bycell.loc[
+            df_orga_bycell["organelle"].eq(organelle)
+          & df_orga_bycell["method"].eq("manual"),
+            "total"
+    ] / df_orga_bycell.loc[
+        df_orga_bycell["organelle"].eq(organelle)
+      & df_orga_bycell["method"].eq("manual"),
+        "total"
+    ].max()
+
 # %%
 names = {
     "peroxisome":   "Peroxisome",
@@ -219,19 +249,20 @@ names = {
     "LD":           "Lipid Droplet",
 }
 correlations = []
+differences = {}
 for organelle in organelles:
     x = df_orga_bycell.loc[
             df_orga_bycell["organelle"].eq(organelle)
           & df_orga_bycell["method"].eq('ilastik'),
-            ["idx-cell","total"]
+            ["idx-cell","total_normed_max"]
         ]
-    x = x / x.max()
+
     y = df_orga_bycell.loc[
             df_orga_bycell["organelle"].eq(organelle)
           & df_orga_bycell["method"].eq('manual'),
-            ['idx-cell',"total"]
+            ['idx-cell',"total_normed_max"]
         ]
-    y = y / y.max()
+
     x.set_index('idx-cell',inplace=True)
     y.set_index('idx-cell',inplace=True)
     xy = x.join(y,how='outer',lsuffix="_x",rsuffix="_y")
@@ -239,9 +270,13 @@ for organelle in organelles:
     # xy_zero.loc[xy_zero["total_x"].isna(),"total_x"] = 0
     # xy_zero.loc[xy_zero["total_y"].isna(),"total_y"] = 0
     xy_drop = xy.dropna()
+    
     corr = np.corrcoef(xy_drop,rowvar=False)
     correlations.append(corr[1,0])
     
+    normed_diff = (xy_drop.loc[:,"total_normed_max_x"] - xy_drop.loc[:,"total_normed_max_y"])/xy_drop.loc[:,"total_normed_max_y"]
+    differences[organelle] = normed_diff
+
     plt.figure()
     plt.title(f"{names[organelle]} Normalized Segmented Total Volume per Cell",fontsize=18)
     plt.xlabel("Ilastik",fontsize=18)
@@ -252,10 +287,29 @@ for organelle in organelles:
     #     [x.min(),x.max()]
     # )
     plt.savefig(f"plots/manual_segment/normalzied-max_scatter_{organelle}.png")
+
+    plt.figure()
+
 plt.figure()
 plt.bar(np.arange(6),correlations)
 plt.xticks(np.arange(6),organelles)
 plt.xlabel("Organelle",fontsize=18)
 plt.ylabel("Correlation Coefficient",fontsize=18)
 plt.savefig(f"plots/manual_segment/normalzied-max_correlation_coefficient.png")
+
+fig, ax = plt.subplots()
+for o,organelle in enumerate(organelles):
+    ax.scatter(
+        o - 0.5 + np.random.random(size=len(differences[organelle])),
+        differences[organelle],
+        s=2,c=cmap(o)
+    )
+bplot = ax.boxplot(
+    [differences[o] for o in organelles],
+    patch_artists = True,
+    tick_labels = organelles
+)
+for p,patch in enumerate(bplot['boxes']):
+    patch.set_facecolor(cmap(p))
+plt.show()
 # %%
